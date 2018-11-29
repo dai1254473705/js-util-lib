84d0f2385fd79b25950bc3d73ebc56a9a9423c89
daiyunzhou <daiyunzhou@zhugefang.com>
2018-11-29 15:07:11 +0800

    添加文件夹

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
-//配置js路径
- //配置js路径
+/*===================================================================
+#    FileName: js-util-lib.js
+#      Author: daiyunzhou
+#       Email: 1254473705@qq.com
+#  CreateTime: 2018-11-29 14:57
+# Description: js-util-lib
+#     Version: v1.0.1
+# Updated by daiyunzhou 2018-11-29 14:57
+===================================================================*/
+
  seajs.config({
     base: "./scripts",
     alias: {
